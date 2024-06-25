32c7430cdb5667d7288fa802016d9f4b2970f7b7
Nick Young <nick.young@auckland.ac.nz>
2024-06-25 14:40:06 +1200

    fastapi

---
+++ API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+from typing import Annotated
+from datetime import datetime
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+
+df = pd.read_csv("questions.csv", index_col=0)
+
+app = FastAPI(root_path="/resbaz24")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/")
+def upload_answer(question: int, answer: str, participant_name: str):
+  assert answer == df.answer[question]
+  pd.DataFrame([{"timestamp": datetime.now(), "participant_name": participant_name, "question": question, "answer": answer}]).to_csv("answers.csv", mode="a", header=False, index=False)
+  return "👍"
