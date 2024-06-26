6564af795178aa8a5504c8656c6265cb8cf8612d
Nick Young <nick.young@auckland.ac.nz>
2024-06-26 00:05:59 +0000

    get endpoint, record wrong answers too, simplify a bit

--- API.py
+++ API.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 
-import os
-from typing import Annotated
-from datetime import datetime
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 import pandas as pd
 
 df = pd.read_csv("questions.csv", index_col=0)
 
-app = FastAPI(root_path="/resbaz24")
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = FastAPI()
+
+@app.get("/")
+def get():
+  return pd.read_csv("answers.csv", names=["timestamp", "participant_name", "question", "answer", "is_correct"]).to_dict(orient="records")
 
 @app.post("/")
-def upload_answer(question: int, answer: str, participant_name: str):
-  if answer != df.answer[question]:
+def post(question: int, answer: str, participant_name: str):
+  is_correct = answer == df.answer[question]
+  pd.DataFrame([{
+    "timestamp": pd.Timestamp.now(),
+    "participant_name": participant_name,
+    "question": question,
+    "answer": answer,
+    "is_correct": is_correct
+  }]).to_csv("answers.csv", mode="a", header=False, index=False)
+
+  if is_correct:
+    return "👍"
+  else:
     return "👎"
-  pd.DataFrame([{"timestamp": datetime.now(), "participant_name": participant_name, "question": question, "answer": answer}]).to_csv("answers.csv", mode="a", header=False, index=False)
-  return "👍"
